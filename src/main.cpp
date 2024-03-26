e7c7475366faab1244cdc7d04a8f69cb11e49d62
LarsBoertjes <larsboertjes@outlook.com>
2024-03-26 19:16:31 +0100

    Starting files and configuration

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+//-- https://github.com/nlohmann/json
+//-- used to read and write (City)JSON
+#include "json.hpp" //-- it is in the /include/ folder
+
+// CGAL libraries
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+
+
+using json = nlohmann::json;
+
+
+
+int main(int argc, const char * argv[]) {
+
+    std::cout << "Test!" << std::endl;
+
+    return 0;
+}
+
