ae8efcb77fdb7232c7d1ed595a70fa398ee911f2
LarsBoertjes <larsboertjes@outlook.com>
2024-03-28 17:26:40 +0100

    Added VoxelGrid structure

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ using namespace std;
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
 typedef K::Point_3 Point3;
 
-// Structures to store vertex and normal data
+// Structures to store all OBJ data
 struct Vertex {
     float x, y, z;
 };
@@ -42,6 +42,45 @@ struct ObjModel {
     vector<Normal> normals;
 };
 
+// Structure to store voxel grid
+struct VoxelGrid {
+    // -- This structure can be used in the following way:
+    // VoxelGrid my_building_grid(rows_x, rows_y, rows_z)
+    // unsigned int v = my_building_grid(x,y,z)
+    // my_building_grid(x,y,z) = 1
+
+    // -- Add functions like:
+    // translate from the model's coordinates to integer coordinates of the voxel grid and vice versa
+
+    std::vector<unsigned int> voxels;
+    unsigned int max_x, max_y, max_z;
+
+    VoxelGrid(unsigned int x, unsigned int y, unsigned int z) {
+        max_x = x;
+        max_y = y;
+        max_z = z;
+        unsigned int total_voxels = x*y*z;
+        voxels.reserve(total_voxels);
+        for (unsigned int i = 0; i < total_voxels; ++i) voxels.push_back(0);
+    }
+
+    unsigned int &operator()(const unsigned int &x, const unsigned int &y, const unsigned int &z) {
+        assert(x >= 0 && x < max_x);
+        assert(y >= 0 && y < max_y);
+        assert(z >= 0 && z < max_z);
+        return voxels[x + y*max_x + z*max_x*max_y];
+    }
+
+    unsigned int operator()(const unsigned int &x, const unsigned int &y, const unsigned int &z) const {
+        assert(x >= 0 && x < max_x);
+        assert(y >= 0 && y < max_y);
+        assert(z >= 0 && z < max_z);
+        return voxels[x + y*max_x + z*max_x*max_y];
+    }
+};
+
+
+
 ObjModel readObjFile(const string& filePath);
 void printModelInfo(const ObjModel& model, bool printGroupDetails);
 
@@ -52,8 +91,6 @@ int main(int argc, const char * argv[]) {
 
 
     // Think about removing the slabs bigger than the building footprint
-
-
 
 
     return 0;
@@ -116,13 +153,14 @@ ObjModel readObjFile(const string& filePath) {
         cout << "Unable to open file" << endl;
     }
 
-    cout << "Successfully read OBJ file in memory with following content: " << endl;
+    cout << "Successfully read " << filePath << " in memory with following content: " << endl;
     printModelInfo(model, false);
 
     return model;
 }
 
 void printModelInfo(const ObjModel& model, bool printGroupDetails) {
+
     cout << "Number of vertices: " << model.vertices.size() << endl;
     cout << "Number of normals: " << model.normals.size() << endl;
     cout << "Number of groups: " << model.groups.size() << endl;
